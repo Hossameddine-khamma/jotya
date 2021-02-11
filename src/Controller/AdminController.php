b5ea5195bfebc8d060c04d89dfaaa4e1d7603cfd
hoss-mks <hoss-mks@github.com>
2021-02-11 11:49:37 +0100

    form produits
    creation du formulaire pour les produits avec la route sur le controller et la vue sur twig

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Produits;
+use App\Form\ProduitsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/admin")
+*/
 class AdminController extends AbstractController
 {
+   
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin")
      */
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+        ]);
+    }
+    /**
+     * @Route("/produits/new", name="add_new_product")
+     */
+    public function addProduct(Request $request): Response
+    {
+        $Manager = $this->getDoctrine()->getManager();
+        $produit = new Produits();
+        $form= $this->createForm(ProduitsType::class,$produit);
+
+        return $this->render('admin/newProduct.html.twig',[
+            'formProduits' => $form->createView()
         ]);
     }
 }
