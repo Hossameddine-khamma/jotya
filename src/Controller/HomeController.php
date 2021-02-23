9d54fcb2f67e188ae06c1048efb2ef4a214bb380
hoss-mks <hoss-mks@github.com>
2021-02-23 15:28:11 +0100

    homeVue
    afficher tous les ensembles:
     - créer un filtre liipImage pour unifier l'affichage
     - button on savoir plus

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Ensembles;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,11 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $em = $this->getDoctrine()->getManager();
+        $rep = $em->getRepository(Ensembles::class);
+        $ensembles = $rep->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'ensembles' => $ensembles,
         ]);
     }
     /**
